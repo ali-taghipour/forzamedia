c0d3077b89735a9b3c04b73c7d0fb9600d3657c0
Ali_Taghipour <taghipourali19@gmail.com>
2021-09-15 04:08:14 +0430

    fix some errors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,8 @@
 import * as React from "react"
 import { Link, graphql, PageProps } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
-import {MarkdownRemark } from "../types/markdown-remark";
 
 
 const BlogIndex = ({ data, location }) => {
@@ -16,7 +13,6 @@ const BlogIndex = ({ data, location }) => {
     return (
       <Layout location={location} title={siteTitle}>
         <Seo title="All posts" />
-        <Bio />
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
@@ -29,7 +25,6 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="All posts" />
-      <Bio />
       <ol style={{ listStyle: `none` }}>
         {posts.map((post) => {
           const title = post.frontmatter.title || post.fields.slug
